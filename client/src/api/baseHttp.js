625587cb5c22e723ecde3447b11082094221106c
Seymur <seymour.aliyev@yandex.ru>
2023-08-21 11:37:28 +0400

    Fixed base http.

--- client/src/api/baseHttp.js
+++ client/src/api/baseHttp.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const baseHttp = axios.create({
-    baseURL: 'http://upload500/api',
+    baseURL: import.meta.env.VITE_API_URL,
     headers: {
         "Content-Type": "application/json"
     }
